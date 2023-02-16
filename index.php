b162682d3d5f3fe31673bfbc83fff0234e9a9f21
Grossmann Jeremy <jeremy.grossmann4@etu.univ-lorraine.fr>
2023-02-16 11:15:30 +0100

    namespaces + index

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
+use App\factory\ConnectionFactory;
+use App\dispatch\Dispatcher;
+use App\exception\DatabaseConnectionException;
 
+session_start();
+
+require_once 'vendor/autoload.php';
+
+ConnectionFactory::setConfig('dbconfig.ini');
+
+$action = $_GET['action'] ?? '';
+
+$dispatcher = new Dispatcher($action);
+
+try {
+    $dispatcher->run();
+} catch (DatabaseConnectionException $exception) {
+    echo $exception->getMessage();
+}
 echo
  "<!DOCTYPE html>
 <html lang='fr'>
