d3b4e6c85bf09274c8b9dfbb7c345dd2f4e99d4e
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2023-02-16 10:28:10 +0100

    index

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 
 echo
  "<!DOCTYPE html>
-<html lang="en">
+<html lang='fr'>
 <head>
-    <meta charset="UTF-8">
+    <meta charset='UTF-8'>
     <title>CrazyCharlyDay</title>
 </head>
 <body>
 
     <header>
-        <a href="">Accueil</a>
-        <a href="">Catalogue</a>
-        <a href="">Mon Panier</a>
-        <a href="">Mon Compte</a>
+        <a href=''>Accueil</a>
+        <a href=''>Catalogue</a>
+        <a href=''>Mon Panier</a>
+        <a href=''>Mon Compte</a>
 
     </header>
 
-<img src="Images/1.jpg" alt="image1">
+<img src='Images/1.jpg' alt='image1'>
 
     <footer>
-        <a href="">Mentions légales</a>
-        <a href="">Nous contacter</a>
+        <a href=''>Mentions légales</a>
+        <a href=''>Nous contacter</a>
     </footer>
 
 </body>
