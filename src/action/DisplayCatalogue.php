f305b0ed4e114b647fb22639a23289bc828e03d6
Lancelot DOAN <lancelot.doan9@etu.univ-lorraine.fr>
2023-02-16 10:41:55 +0100

    display

---
+++ src/action/DisplayCatalogue.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\action;
+
+use Application\action\Action;
+
+class DisplayCatalogue extends Action
+{
+
+    public function execute(): string
+    {
+        // TODO: Implement execute() method.
+    }
+}
