b99c2118f5f8f6d5a428f02dd5633dd217e08dc7
Thomas <thomas.boillot@etu.univ-lorraine.fr>
2023-02-16 15:39:51 +0100

    Filter options

--- src/action/DisplayCatalogue.php
+++ src/action/DisplayCatalogue.php
@@ -8,10 +8,45 @@ class DisplayCatalogue extends Action
 
     public function execute(): string
     {
-        $html = " <form action='index.php?action=catalogue' method='post' id='searchbar'>
+        $html = " <form action='?action=catalogue' method='post' id='searchbar'>
                     <input type='text' name='search' placeholder='Rechercher'>
                     <input type='submit' value='Rechercher'>
-                </form>";
+                  </form>
+                
+                <div id='filter'>
+                <form method='POST' action='?action=catalogue'>
+                <label for='ville'>Filtrer par ville</label>
+                <br> <select name='ville'>
+                    <option><button value ='Chauny'>Chauny</button></option>
+                    <option><button value ='Les Pennes-Mirabeau'>Les Pennes-Mirabeau</button></option>
+                    <option><button value='Leyr'>Leyr</button></option>
+                    <option><button value='Lucey'>Lucey</button></option>
+                    <option><button value='Nancy'>Nancy</button></option>
+                    <option><button value='Pont à Mousson'>Pont à Mousson</button></option>
+                    <option><button value='Santeny'>Santeny</button></option>
+                    <option><button value='Sarralbe'>Sarralbe</button></option>
+                    <option><button value='Villeurbanne'>Villeurbanne</a></option>
+                    <option><button value='Wiwersheim'>Wiwersheim</button></option>
+                </select>
+                <br><button type='submit'>Filtrer par ville</button>
+                </form>
+                
+                
+             
+                <form method='POST' action='?action=catalogue'>
+                <label for='categorie'>Filtrer par catégorie</label>
+                <br><select name='categorie'>
+                    <option><button value ='Épicerie'>Épicerie</button></option>
+                    <option><button value ='Boissons'>Boissons</button></option>
+                    <option><button value='Droguerie'>Droguerie</button></option>
+                    <option><button value='Cosmétiques'>Cosmétiques</button></option>
+                    <option><button value='Produits frais'>Produits frais</button></option>
+                </select>
+                <br><button type='submit'>Filtrer par catégorie</button>
+                </form>
+                </div>
+                
+";
 
 
 
@@ -20,12 +55,15 @@ class DisplayCatalogue extends Action
             $req = $db->prepare($query);
             $req->execute();
             $nbPage = $req->fetchColumn();
+            $ville = $_POST['ville'] ?? '';
+            $categorie = $_POST['categorie'] ?? '';
+
             if (isset($_POST['search'])) {
                 $recherche = $_POST['search'];
                 $recherche = strtolower($recherche);
-                $query = "SELECT id,nom,prix,lieu,img FROM produit WHERE nom LIKE '%$recherche%'";
+                $query = "SELECT produit.id,produit.nom,prix,lieu,img FROM produit inner join categorie on produit.categorie = categorie.id WHERE nom LIKE '%$recherche%' and lieu LIKE '%$ville%' and categorie.nom LIKE '%$categorie%'";
             } else {
-                $query = "SELECT id,nom,prix,lieu,img FROM produit";
+                $query = "SELECT produit.id,produit.nom,prix,lieu,img FROM produit inner join categorie on produit.categorie = categorie.id where lieu LIKE '%$ville%' and categorie.nom LIKE '%$categorie%'";
             }
 
 
